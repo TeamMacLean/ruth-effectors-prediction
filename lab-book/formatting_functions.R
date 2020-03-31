81b58bf03c820e0c11a7de0f0766be7c2b33bd7c
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2020-03-31 22:07:07 +0100

    Renamed scripts to consistent filenames (- to _)

---
+++ lab-book/formatting_functions.R
@@ -0,0 +1,58 @@
+get_formatted_date <- function(curr_date, to_date = NULL, range = FALSE, comment = NULL) {
+  if (is.null(to_date)) {
+    date_text <- curr_date %>%
+      lubridate::ymd() %>%
+      base::format("%d %B %Y (%A)")
+
+    date_text <- paste(
+      date_text,
+      ifelse(
+        is.null(comment),
+        "",
+        paste("–", stringr::str_to_title(comment))
+      )
+    )
+  } else {
+    days_list <- seq(as.Date(curr_date), as.Date(to_date), 1)
+    if (!range) {
+      days_text <- days_list %>%
+        lubridate::ymd() %>%
+        base::format("%d") %>%
+        stringr::str_c(collapse = ", ")
+
+      weekdays_text <- days_list %>%
+        lubridate::ymd() %>%
+        base::format("%A") %>%
+        stringr::str_c(collapse = ", ")
+
+      weekdays_text <- paste0(" (", weekdays_text, ") ")
+    } else {
+      days_text <- days_list %>%
+        lubridate::ymd() %>%
+        base::format("%d")
+
+      days_text <- days_text[c(1, length(days_text))] %>%
+        stringr::str_c(collapse = "–")
+
+      weekdays_text <- ""
+    }
+
+    monthyear_text <- days_list %>%
+      lubridate::ymd() %>%
+      base::format("%B %Y") %>%
+      unique()
+
+    date_text <- paste0(
+      days_text, " ",
+      monthyear_text,
+      weekdays_text,
+      ifelse(
+        is.null(comment),
+        "",
+        paste("–", stringr::str_to_title(comment))
+      )
+    )
+  }
+
+  return(date_text)
+}
